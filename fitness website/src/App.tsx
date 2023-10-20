17f9bdc0aada0d9bc4c56ec98f0790c8ea1818f7
dCOMMICS <sanwurld@gmail.com>
2023-10-21 01:25:29 +0300

    making some correction

--- fitness website/src/App.tsx
+++ fitness website/src/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
 
-import viteLogo from '/vite.svg'
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  
+ 
+  return(
+
+  <div className="app">app
+
+  </div>
   )
 }
 
